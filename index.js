c53dffccd0ad2d4646305995b0784842bce864d5
insr531 <insoorhee@hotmail.com>
2022-01-26 17:56:27 -0800

    process env node env

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname,"./client/build")));
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+}
 
 const port = process.env.PORT || 8080;
 
